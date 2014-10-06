689a80287047145e07ebf8e98a0eefaf3c141018
otknoy <otknoy@gmail.com>
2014-10-06 19:34:23 +0900

    Minor change

--- nlp/tfidf.py
+++ nlp/tfidf.py
@@ -9,14 +9,15 @@ def term_frequency(term_list):
         tf[t] += 1
     return tf
 
-def document_frequency(docs):
+def document_frequency(texts):
     df = {}
-    for terms in docs:
+    for terms in texts:
         for t in list(set(terms)):
             if not df.has_key(t):
                 df[t] = 0
             df[t] += 1
     return df
+
 
 if __name__ == '__main__':
     texts = [u'Emacs（イーマックス）とは高機能でカスタマイズ性の高いテキストエディタである。スクリーン・エディタとしての人気が高く、特にUNIXのプログラマを中心としたコンピュータ技術者に愛用者が多い。',
