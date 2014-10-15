e7f4e9248b999e7eea63ef25118129be654b5bff
otknoy <otknoy@gmail.com>
2014-10-15 17:39:25 +0900

    Simplify

--- visualization/wordcloud/js/wordcloud.js
+++ visualization/wordcloud/js/wordcloud.js
@@ -2,66 +2,59 @@ var selector = '#wordcloud';
 var width = 1024;
 var height = 768;
 
-var words = [];
+var filename = 'data/data.csv';
 
-var layout = d3.layout.cloud()
-	.timeInterval(10)
-	.size([width, height])
-	.padding(5)
-	.rotate(function() { return ~~(Math.random() * 2) * 90; })
-	.font("Impact")
-	.fontSize(function(d) { return d.size; })
-	.on("word", progress)
-	.on("end", draw);
-
-var svg = d3.select(selector).append("svg")
-	.attr("width", width)
-	.attr("height", height);
-// var background = svg.append("g");
-var vis = svg.append("g")
-	.attr("transform", "translate(" + [width >> 1, height >> 1] + ")");
-
-function progress(data) {
-    // console.log(data);
-}
-
-function draw(data, bounds) {
-    var fill = d3.scale.category20();
-
-    vis.selectAll("text")
-    	.data(data)
-    	.enter()
-    	.append("text")
-    	.style({
-    	    "font-size": function(d) { return d.size + "px"; },
-    	    "font-family": "Impact",
-    	    "fill": function(d, i) { return fill(i); }
-    	})
-    	.attr({
-    	    "text-anchor": "middle",
-    	    "transform": function(d) { return "translate(" + [d.x, d.y] + ") rotate(" + d.rotate + ")"; }
-    	})
-    	.text(function(d) { return d.text; });
-}
-
-function loadFile(filename) {
-    d3.csv(filename, function(data) {
-	data = data.splice(0, 500);
-	var words = data.map(function(d) {
-	    return {
-		text: d.word,
-		size: 3*Math.pow(d.count, 1.2)
-	    };
-	});
-	layout.stop().words(words).start();
+d3.csv(filename, function(data){
+    data = data.splice(0, 1000);
+    var wordcount = data.map(function(d) {
+	return {
+	    text: d.word,
+	    // size: Math.pow(d.count/2, 1.1)//2*Math.pow(d.count, 1.4)
+	    size: 3*Math.pow(d.count, 1.2)
+	};
     });
-}
 
 
-var filename = $('select option:selected').val();
-loadFile(filename);
+    var svg = d3.select(selector).append("svg")
+	    .attr("width", width)
+	    .attr("height", height);
+    var vis = svg.append("g")
+	    .attr("transform", "translate(" + [width >> 1, height >> 1] + ")");
 
-$('#file-select').change(function() {
-    var filename = $('select option:selected').val();
-    loadFile(filename);
+    var layout = d3.layout.cloud()
+	    .size([width, height])
+            .words(wordcount)
+            .padding(5)
+	    .rotate(function() { return 0;})
+	    // .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .font("Impact")
+            .fontSize(function(d) { return d.size; })
+	    .on("word", progress)
+            .on("end", draw)
+	    .start();
+
+
+    function progress(data) {
+	console.log(progress);
+    }
+
+    function draw(words) {
+	var fill = d3.scale.category20();
+
+        vis.selectAll("text")
+            .data(words)
+            .enter()
+            .append("text")
+            .style({
+		"font-size": function(d) { return d.size + "px"; },
+		"font-family": "Impact",
+		"fill": function(d, i) { return fill(i); }
+            })
+            .attr({
+		"text-anchor": "middle",
+		"transform": function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; }
+            })
+            .text(function(d) { return d.text; });
+    }
 });
+
